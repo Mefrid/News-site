8f5297fd69b313fce08fc7ffd839e89e5d679907
Михаил Баженов <bazhenov.m@spectrumdata.ru>
2022-10-10 17:00:05 +0600

    updated readme

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import AboutPage from "../src/components/AboutPage/AboutPage";
 
 const About: NextPage = () => (
   <>
     <Head>
       <title>Сайт новостей - О нас</title>
     </Head>
+    <AboutPage />
   </>
 );
 
---
+++ src/components/AboutPage/AboutPage.tsx
@@ -0,0 +1,67 @@
+import { FC } from "react";
+import styles from "./AboutPage.module.scss";
+
+const AboutPage: FC = () => (
+  <div className={styles.container}>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt esse ut
+      incidunt nam molestiae quo doloremque accusantium odio quos? Dolore magni
+      rerum numquam exercitationem ipsam quaerat, facilis assumenda sunt eos
+      maxime eaque accusamus earum, harum unde atque nemo molestias consequuntur
+      eius culpa minus sapiente eum labore sit ratione. Dolorum pariatur, ullam
+      est assumenda tempora optio provident repellat dolores. Provident, atque
+      error? Quae repudiandae debitis totam non, quidem veritatis. Sint,
+      praesentium laborum fuga id enim voluptate eaque aspernatur maiores alias
+      inventore quod totam nobis provident doloribus distinctio culpa, assumenda
+      expedita optio odit. In soluta neque veniam ipsum. Nesciunt, adipisci!
+      Commodi quidem debitis ea dolore quibusdam adipisci, necessitatibus totam
+      ratione nihil excepturi! Eveniet quidem in, deserunt itaque ex magnam
+      optio expedita, animi assumenda odio ipsa dolore officiis libero vero
+      ipsam neque id magni! Labore numquam officia asperiores expedita sequi
+      quae mollitia accusamus veritatis aut, dolor tenetur libero eaque totam
+      sint maiores iure. Vero laboriosam ad obcaecati voluptas minus sapiente
+      officiis atque, amet repudiandae nemo itaque ratione quidem. Libero
+      officia modi commodi fugiat ex exercitationem architecto voluptas ipsam
+      quia impedit, sed qui animi ut porro obcaecati cum id aliquid, soluta
+      cupiditate velit dolorem, possimus officiis eligendi? Architecto est
+      beatae iusto nulla illo aliquid doloremque aspernatur dolorem omnis
+      laborum, a hic laudantium fuga vero porro sequi nobis nam saepe provident
+      nihil distinctio recusandae? Quam deserunt, saepe ullam ea veritatis earum
+      reprehenderit facilis itaque quibusdam sequi soluta fugiat eum molestiae
+      inventore incidunt commodi, dolorum architecto iusto labore veniam
+      voluptatibus, voluptate pariatur aperiam. Repellendus, soluta consequatur?
+      Deserunt accusamus nostrum neque magnam quidem, ducimus labore,
+      consequuntur ipsa laborum ut assumenda atque vero illum omnis minima
+      dolore officia, minus quod ab. Dolore quod inventore eveniet magni ipsam
+      eos dolores voluptatibus perferendis laudantium vitae aperiam neque nam
+      commodi, officiis, asperiores fugiat ratione quia vel itaque, ad totam
+      voluptate eligendi porro. Maiores repellat impedit vero quam, fugiat quos
+      nam velit, exercitationem magnam sed alias consequuntur neque ad,
+      dignissimos dolorum temporibus fuga odit ipsa voluptatum facilis minima
+      eveniet aperiam.
+    </p>
+    <p>
+      Hic quidem ipsum qui pariatur veritatis ullam modi nulla similique et
+      laudantium doloremque ea inventore numquam illo, facere eos cum,
+      distinctio id nihil? Facilis ex corrupti consequatur placeat! Omnis
+      dolorum, voluptas non alias ex vero laudantium, fugiat facere distinctio
+      repellendus sunt quo ducimus in, ea perferendis qui incidunt maxime a vel
+      consectetur voluptates! Blanditiis minima itaque consequatur labore natus
+      sapiente nisi, dolorum maiores neque delectus nam perspiciatis amet
+      impedit dolorem beatae quo. At nostrum, id inventore temporibus
+      perspiciatis odio sapiente enim expedita recusandae. Nostrum enim,
+      repellendus in temporibus distinctio harum quis dignissimos molestias
+      autem exercitationem voluptatum maiores dolorum neque soluta, cumque omnis
+      illo perferendis perspiciatis nemo explicabo accusamus atque at.
+      Consequatur officiis omnis vel quibusdam, nisi officia accusantium natus
+      facilis, molestiae minus ex iusto reprehenderit. Possimus quia quas et
+      totam iure saepe doloribus cumque dolor recusandae! Ad nobis quis
+      quibusdam dolores, optio magnam eaque officiis, corrupti ipsum provident
+      adipisci tempore, officia distinctio at quae expedita doloremque dolore!
+      Culpa ab in dignissimos blanditiis explicabo dolore consequuntur quod
+      ullam iste? Ipsa perspiciatis tempore beatae.
+    </p>
+  </div>
+);
+
+export default AboutPage;
